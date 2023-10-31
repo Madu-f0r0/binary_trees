261dd04b05ad0393a06225f15ea13684c375bde7
Madu-f0r0 <duherbert@gmail.com>
2023-10-31 13:06:06 +0100

    Refactored  variables; completed  and  fxns

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_leaves - counts the number of leaves in a binary tree
+ * @tree: a pointer to the root node of the tree
+ *
+ * Return: The number of leaves in the tree; 0 if the tree is NULL
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t leaves = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		leaves++;
+
+	leaves += binary_tree_leaves(tree->left);
+	leaves += binary_tree_leaves(tree->right);
+
+	return (leaves);
+}
